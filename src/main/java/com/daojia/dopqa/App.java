23b5ec9dce88812fdc14e3e4b23b4c435ee983f2
shensl <shensl@daojia.com>
2018-11-30 16:15:23 +0800

    init project

---
+++ src/main/java/com/daojia/dopqa/App.java
@@ -0,0 +1,13 @@
+package com.daojia.dopqa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
